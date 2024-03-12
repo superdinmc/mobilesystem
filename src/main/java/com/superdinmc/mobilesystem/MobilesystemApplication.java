22558dcb0dc6b716fb94d6ac8e6dbbc1baa4707d
superdinmc <superdinmc@gmail.com>
2024-03-12 14:33:21 +0700

    .

--- src/main/java/com/superdinmc/mobilesystem/MobilesystemApplication.java
+++ src/main/java/com/superdinmc/mobilesystem/MobilesystemApplication.java
@@ -2,11 +2,11 @@ package com.superdinmc.mobilesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.superdinmc.mobilesystem.controller.HomeController;
 
 @SpringBootApplication
 public class MobilesystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MobilesystemApplication.class, args);
 	}
-
 }

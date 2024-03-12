1155aa8e123b1e18d6bce1ae2bdbfa6c31db4b59
superdinmc <superdinmc@gmail.com>
2024-03-12 13:11:45 +0700

    Initial Commit

---
+++ src/main/java/com/superdinmc/mobilesystem/MobilesystemApplication.java
@@ -0,0 +1,12 @@
+package com.superdinmc.mobilesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilesystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/superdinmc/mobilesystem/MobilesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superdinmc.mobilesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

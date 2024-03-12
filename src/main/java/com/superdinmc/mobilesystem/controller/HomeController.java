aa610581e280b8a3208614b7ca088d669e6fedd1
superdinmc <superdinmc@gmail.com>
2024-03-12 15:34:29 +0700

    .

--- src/main/java/com/superdinmc/mobilesystem/controller/HomeController.java
+++ src/main/java/com/superdinmc/mobilesystem/controller/HomeController.java
@@ -11,4 +11,9 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+    @GetMapping("/AllProducts")
+    public String AllProduct() {
+        return "all-products";
+    }
 }
